42261122f369fffe35109a742bfd7f4af3c9fdfd
zakiguedi <zaki.guedi@gmail.com>
2023-02-16 19:56:55 -0500

    ISS spotter assignment

---
+++ iss.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation };
